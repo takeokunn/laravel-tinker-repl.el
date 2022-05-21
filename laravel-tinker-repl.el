03e82f6240238564e4e8f8db4348f1aad7889bcb
takeokunn <bararararatty@gmail.com>
2022-05-21 13:25:58 +0900

    Update

--- laravel-tinker-repl.el
+++ laravel-tinker-repl.el
@@ -30,8 +30,10 @@
 ;;; Code:
 
 
-(require 'comint)
 (require 'f)
+(require 'comint)
+(require 'php-mode)
+(require 'ansi-color)
 
 (defconst laravel-tinker-repl-version "1.0.0"
   "laravel tinker repl version number.")
@@ -42,11 +44,12 @@
   :prefix "laravel-tinker-repl-"
   :group 'laravel-tinker-repl-repl)
 
-(define-derived-mode laravel-tinker-repl-mode comint-mode "Laravel tinker REPL"
-  "Major-mode for laravel Tiner REPL.")
-
 (defvar laravel-tinker-repl-mode-map
   (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "TAB") 'completion-at-point)
+    (define-key map (kbd "C-c C-c") 'laravel-tinker-repl-quit-or-cancel)
+    (define-key php-mode-map (kbd "C-c C-c") 'laravel-tinker-repl-send-line)
+    (define-key php-mode-map (kbd "C-c C-z") 'laravel-tinker-repl-switch-to-repl)
     map))
 
 (defvar laravel-tinker-repl-mode-hook
@@ -59,10 +62,28 @@
   :tag "laravel tinker repl execution command"
   :type '())
 
+(defvar laravel-tinker-repl-process-name "laravel-tinker"
+  "process name of laravel tinker REPL.")
+
+(defcustom laravel-tinker-repl-input-ignoredups t
+  "If non-nil, don't add input matching the last on the input ring. See also `comint-input-ignoredups'"
+  :group 'laravel-tinker-repl
+  :type 'boolean)
+
+(defcustom laravel-tinker-repl-process-echoes t
+  "If non-nil, laravel tinker does not echo any input. See also `comint-process-echoes'"
+  :group 'laravel-tinker-repl
+  :type 'boolean)
+
 (defvar laravel-tinker-repl-comint-buffer-process
   nil
   "A list (buffer-name process) is arguments for `make-comint'.")
 (make-variable-buffer-local 'laravel-tinker-repl-comint-buffer-process)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;            private method           ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun laravel-tinker-repl--detect-buffer ()
   "Return tuple list, comint buffer name and program."
@@ -77,10 +98,40 @@
          nil
          (cdr laravel-tinker-repl-exec-command)))
 
+(defun laravel-tinker-repl--get-or-create-process ()
+  (let ((proc (get-process laravel-tinker-repl-process-name)))
+    (unless (processp proc)
+      (save-excursion (laravel-tinker-repl))
+      (setq proc (get-process laravel-tinker-repl-process-name)))
+    proc))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;           public function           ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defun laravel-tinker-repl-quit-or-cancel ()
+  "Send ^C to laravel tinker repl process."
+  (interactive)
+  (process-send-string (get-process laravel-tinker-repl-comint-buffer-process) "\x03"))
+
+;;;###autoload
+(defun laravel-tinker-repl-send-line ()
+  (interactive)
+  (let ((str (thing-at-point 'line 'no-properties)))
+    (comint-send-string (cadr (laravel-tinker-repl--detect-buffer)) str)))
+
+;;;###autoload
+(defun laravel-tinker-repl-switch-to-repl ()
+  "If there is a `laravel-tinker-repl-process' running switch to it, otherwise spawn one."
+  (interactive)
+  (pop-to-buffer
+   (process-buffer (laravel-tinker-repl--get-or-create-process))))
+
 ;;;###autoload
 (defun laravel-tinker-repl ()
   (interactive)
-  (let* ((buf-name "laravel-tinker-repl")
+  (let* ((buf-name laravel-tinker-repl-process-name)
          (my-dir (read-directory-name "DIRECTORY: "))
          (default-directory my-dir))
     (switch-to-buffer (laravel-tinker-repl--make-process))
@@ -93,11 +144,12 @@
   (let ((laravel-tinker-repl-comint-buffer-process (list buf-name process)))
     (call-interactively 'laravel-tinker-repl)))
 
-;;;###autoload
-(defun laravel-tinker-repl-send-line ()
-  (interactive)
-  (let ((str (thing-at-point 'line 'no-properties)))
-    (comint-send-string (cadr (laravel-tinker-repl--detect-buffer)) str)))
+(define-derived-mode laravel-tinker-repl-mode comint-mode "Laravel tinker REPL"
+  "Major-mode for laravel Tiner REPL."
+  :syntax-table php-mode-syntax-table
+  (setq comint-input-ignoredups laravel-tinker-repl-input-ignoredups)
+  (setq comint-process-echoes laravel-tinker-repl-process-echoes)
+  (ansi-color-for-comint-mode-on))
 
 (provide 'laravel-tinker-repl)
 
